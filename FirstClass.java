e9a00ac1e2c341eafc7ff83b6aedb5f1ae6c0bb7
tulc <2285389867@qq.com>
2020-07-19 15:44:06 +0800

    首次提交

---
+++ FirstClass.java
@@ -0,0 +1,17 @@
+package com.luchang.redis;
+
+/**
+ * 第一个类
+ *
+ * @VERSION V1.0
+ * @DESCRIPTION com.luchang.redis
+ * @AUTH luchang
+ * @DATE 2020/7/19
+ */
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello redis");
+    }
+
+}
